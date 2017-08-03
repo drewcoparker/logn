3e3c914e5e453ce232711d43b5026d95aecd7247
Drew Parker <drew.i.parker@gmail.com>
2017-08-02 22:07:56 -0400

    Added redux and set up index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,41 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux and Router Imports
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import reduxPromise from 'redux-promise';
+import reducers from './reducers/index.js';
+
+// A token is generated from the backend when the user logs in.
+// loadState and saveState are methods to save to and retrieve this token to
+// local storage. This allows the user to reamin logged in while navigating
+// througout the app and even after she leaves closes the browser and returns
+// to the page.
+import { loadState, saveState } from './localStorage';
+const persistedState = loadState();
+
+// Instantiate the store obect with createStore method. The reducers param is
+// required. I also pass the login token retreived from locals storage as
+// persistedState. Finally, reduxPromise middleware is passed to assist in my
+// ajax requests.
+const store = createStore(
+    reducers,
+    persistedState,
+    applyMiddleware(
+        reduxPromise
+    )
+)
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
