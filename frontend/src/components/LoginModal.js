771b553ed8e8a0b88d1bde33eca5a35fc6e02611
Drew Parker <drew.i.parker@gmail.com>
2017-08-20 00:10:51 -0400

    Added a close modal button

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -20,14 +20,19 @@ class LoginModal extends Component {
         return (
             <div className="modal-state">
                 <div className="modal">
-                    <div className="modal-header">
-                        <h1>Login</h1>
-                    </div>
-                    <div className="modal-body">
-                        <form>
-                            <input id="email-input" placeholder="email address" type="text" />
-                            <input id="password-input" placeholder="password" type="text" />
-                        </form>
+                    <div className="modal-wrapper">
+                        <div className="button-wrapper" onClick={this.closeModal}>
+                            <div className="close-modal-button"></div>
+                        </div>
+                        <div className="modal-header">
+                            <h1>Login</h1>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <input id="email-input" placeholder="Email address" type="text" />
+                                <input id="password-input" placeholder="Password" type="text" />
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div className="overlay" onClick={this.closeModal}></div>
