a150a06191996fde9db84a1324a03d46e8eabb54
Drew Parker <drew.i.parker@gmail.com>
2017-08-19 23:27:41 -0400

    Styled inputs in modal, added header

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -17,37 +17,20 @@ class LoginModal extends Component {
         if (!this.props.show)
             return null;
 
-        let modalStyle = {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            width: '410px',
-            maxWidth: '100%',
-            height: '450px',
-            maxHeight: '100%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: '9999',
-            animationName: 'slideUpLarge',
-            animationFillMode: 'forwards',
-            animationDelay: '.5s',
-            animationTimingFunction: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
-            background: '#fff'
-        }
-
-        let backdropStyle = {
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            top: '0px',
-            left: '0px',
-            zIndex: '9998',
-            background: 'rgba(0, 0, 0, 0.3)'
-        }
-
         return (
             <div className="modal-state">
-                <div style={modalStyle}></div>
-                <div style={backdropStyle} onClick={this.closeModal}></div>
+                <div className="modal">
+                    <div className="modal-header">
+                        <h1>Login</h1>
+                    </div>
+                    <div className="modal-body">
+                        <form>
+                            <input id="email-input" placeholder="email address" type="text" />
+                            <input id="password-input" placeholder="password" type="text" />
+                        </form>
+                    </div>
+                </div>
+                <div className="overlay" onClick={this.closeModal}></div>
             </div>
         );
     }
