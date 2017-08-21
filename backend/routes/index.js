0bd58b6f120a48f0319522fb49420d436461d2c0
Drew Parker <drew.i.parker@gmail.com>
2017-08-20 21:40:23 -0400

    Set up express with mongo and created a login route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10,
+    mongoose = require('mongoose'),
+    server = 'mongodb://localhost:27017/logn';
+
+// Mongo connection
+mongoose.connect(server, (err) => {
+    if (err) throw err;
+    console.log(`\nConnected to ${server}`);
+});
+
+// Define a schema
+var Schema = mongoose.Schema,
+    userDataSchema = new Schema({
+        email: { type: String, required: true, index: { unique: true} },
+        password: { type: String, required: true },
+    }, { collection: 'users' }),
+    userData = mongoose.model('UserData', userDataSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Get all users route (never called by the frontend)
+router.get('/get-users', (req, res, next) => {
+  userData.find()
+    .then((docs) => {
+        console.log(docs);
+    })
+});
+
+// Login router
+router.get('/login', (req, res, next) => {
+    var email = req.body.email,
+        password = req.body.password;
+
+    userData.findOne({email: email})
+        .then((doc) => {
+            res.json({
+                login: true,
+                name: doc.firstName
+            });
+        })
+        .catch((err) => {
+            throw err;
+            res.json({
+                msg: 'failed to login'
+            });
+        })
 });
 
 module.exports = router;
