fd22e54782044c90f413e7df3b4f0bf3d9b83fb3
Drew Parker <drew.i.parker@gmail.com>
2017-08-27 18:04:19 -0400

    Register modal working.

--- frontend/src/components/RegisterModal.js
+++ frontend/src/components/RegisterModal.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import RegisterModalAction from '../actions/RegisterModalAction';
 
 class RegisterModal extends Component {
     constructor(props) {
@@ -6,6 +9,11 @@ class RegisterModal extends Component {
         this.state = {
             msg: ''
         }
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    closeModal() {
+        this.props.dispatchModal(false);
     }
 
     render() {
@@ -18,20 +26,20 @@ class RegisterModal extends Component {
             <div className="modal-state">
                 <div className="modal">
                     <div className="modal-wrapper">
-                        <div className="button-wrapper">
+                        <div className="button-wrapper" onClick={this.closeModal}>
                             <div className="close-modal-button"></div>
                         </div>
                         <div className="modal-header">
-                            <h1>Login</h1>
+                            <h1>Register</h1>
                         </div>
                         <div className="modal-body">
                             <form>
                                 <input id="email-input" placeholder="Email Address" type="text" />
                                 <input id="password-input" placeholder="Password (case sensitive)" type="password" />
-                                <button className="login-submit-btn" type="submit">Login</button>
+                                <button className="login-submit-btn" type="submit">Submit</button>
                             </form>
                             <div className="login-info">
-                                <div className="login-info-header"><span>Login Response:</span></div>
+                                <div className="login-info-header"><span>Response:</span></div>
                                 <div className="login-info-body">
                                     <span></span>
                                 </div>
@@ -39,10 +47,23 @@ class RegisterModal extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="overlay"></div>
+                <div className="overlay" onClick={this.closeModal}></div>
             </div>
         );
     }
 }
 
-export default RegisterModal
+function mapStateToProps(state) {
+    return {
+        loginResponse: state.loginResponse
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        dispatchModal: RegisterModalAction,
+        // registerSubmit: RegisterAction
+    }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(RegisterModal);
